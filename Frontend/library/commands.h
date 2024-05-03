6251502fe443b67d241f5e2219711a5944fb257e
Vladimir1t <vova.vexov@mail.ru>
2024-05-03 19:36:12 +0300

    add new function

--- Frontend/library/commands.h
+++ Frontend/library/commands.h
@@ -1,7 +1,7 @@
 #ifndef COMMANDS_H_INCLUDED
 #define COMMANDS_H_INCLUDED
 
-const int MAX_OP_SIZE  = 4;
+const int MAX_OP_SIZE  = 5;
 const int MAX_STR_SIZE = 20;
 
 enum Class_type
@@ -19,30 +19,48 @@ enum Class_type
     DEFUALT   = -1,
 };
 
-enum Class_operation
+enum token_t
 {
-    OP_ADD = 1,
-    OP_SUB = 2,
-    OP_MUL = 3,
-    OP_DIV = 4,
-    OP_SIN = 5,
-    OP_COS = 6,
-    OP_TG  = 7,
-    OP_CTG = 8,
-    OP_LN  = 9,
-    OP_SH  = 10,
-    OP_CH  = 11,
-    OP_DEG = 12,
-    OP_EXP = 13
+    OP    = 1,
+    KEY_W = 2,
+    NUM   = 3,
+    VAR   = 4,
+    OP_L  = 5,
+    BR_C  = 6,
+    BR_O  = 7,
+    CBR_C = 8,
+    CBR_O = 9,
+    IF_   = 10,
+    FUNC  = 11,
+    SIGN  = 12,
+    END   = 13,
 };
 
-enum Class_key_word
+enum Class_operation
+{
+    OP_ADD  = 1,
+    OP_SUB  = 2,
+    OP_MUL  = 3,
+    OP_DIV  = 4,
+    OP_SIN  = 5,
+    OP_COS  = 6,
+    OP_TG   = 7,
+    OP_CTG  = 8,
+    OP_LN   = 9,
+    OP_SH   = 10,
+    OP_CH   = 11,
+    OP_DEG  = 12,
+    OP_EXP  = 13,
+    OP_SQRT = 14,
+};
+
+/*enum Class_key_word
 {
     equal    = '=',
     skobe_op = '{',
     skobe_cl = '}',
     str_end  = ';',
-};
+};*/
 
 struct element
 {
@@ -52,19 +70,20 @@ struct element
 
 static struct element array_op[] =
 {
-    {"+",   OP_ADD},
-    {"-",   OP_SUB},
-    {"*",   OP_MUL},
-    {"/",   OP_DIV},
-    {"sin", OP_SIN},
-    {"cos", OP_COS},
-    {"tg",  OP_TG},
-    {"ctg", OP_CTG},
-    {"ln",  OP_LN},
-    {"sh",  OP_SH},
-    {"ch",  OP_CH},
-    {"^",   OP_DEG},
-    {"exp", OP_EXP}
+    {"+",    OP_ADD},
+    {"-",    OP_SUB},
+    {"*",    OP_MUL},
+    {"/",    OP_DIV},
+    {"sin",  OP_SIN},
+    {"cos",  OP_COS},
+    {"tg",   OP_TG},
+    {"ctg",  OP_CTG},
+    {"ln",   OP_LN},
+    {"sh",   OP_SH},
+    {"ch",   OP_CH},
+    {"^",    OP_DEG},
+    {"exp",  OP_EXP},
+    {"sqrt", OP_SQRT}
 };
 const int OP_NUM = sizeof (array_op) / sizeof (array_op[0]);
 
