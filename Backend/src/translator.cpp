dae5ab9fa3b9bd1d50fc67cf79c3a4c7e003efe6
Vladimir1t <vova.vexov@mail.ru>
2024-05-04 15:02:06 +0300

    Update translator.cpp

--- Backend/src/translator.cpp
+++ Backend/src/translator.cpp
@@ -1,6 +1,15 @@
 #include "..\include\translator.h"
 
-int main ()
+int run_translator (struct Node* root, char* asm_file)
 {
-    
+    CHECK_PTR (root);
+    CHECK_PTR (asm_file);
+
+    FOPEN (asm_file_p, asm_file, "w");
+
+    struct Variables variables = {0};
+    make_asm_code (root, &variables);
+
 }
+
+int make_asm_code (struct Node* root, struct Variables)
