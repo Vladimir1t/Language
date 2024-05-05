0ad35ae80575858cd22629766ef18c9edea57835
Vladimir1t <vova.vexov@mail.ru>
2024-05-05 16:14:14 +0300

    add macroses of input_output.cpp

--- Backend/library/library_of_defines.h
+++ Backend/library/library_of_defines.h
@@ -51,24 +51,34 @@
     while (0)
 
 #define INCREASE_CAPACITY(...)                                              \
-    tokens->capacity *= 2;                                                  \
-    REALLOC (tokens->array_tokens, struct Token, tokens->capacity);
+    variables->capacity *= 2;                                               \
+    REALLOC (variables->array_var, struct Variable, variables->capacity);   \
 
-#define ADD_LONG_OP(...)                                                            \
-    tokens->array_tokens[tokens->size].type = OP_L;                                 \
-    CALLOC (tokens->array_tokens[tokens->size].data.op_long, char, MAX_STR_SIZE);   \
-    strcpy (tokens->array_tokens[tokens->size].data.op_long, str);                  \
-    printf ("[%s]\n", tokens->array_tokens[tokens->size].data.op_long);             \
-    tokens->size += 1;                                                              \
-    break;
+#define ADD_LONG_OP(...)                                        \
+    {                                                           \
+        char str[MAX_STR_SIZE] = {0};                           \
+        int j = 0;                                              \
+        while (isalpha (text_data[*i]))                         \
+        {                                                       \
+            str[j++] = text_data[*i];                           \
+            *i += 1;                                            \
+        }                                                       \
+        CALLOC (node->data.op_long, char, MAX_STR_SIZE);        \
+        strcpy (node->data.op_long, str);                       \
+    }
 
-#define ADD_FUNCTION(...)                                                           \
-    tokens->array_tokens[tokens->size].type = FUNC;                                 \
-    CALLOC (tokens->array_tokens[tokens->size].data.func, char, MAX_STR_SIZE);      \
-    strcpy (tokens->array_tokens[tokens->size].data.func, str);                     \
-    printf ("func [%s]\n", tokens->array_tokens[tokens->size].data.func);           \
-    tokens->size += 1;                                                              \
-    break;
+#define ADD_FUNCTION(...)                                           \
+    {                                                               \
+        char str[MAX_STR_SIZE] = {0};                               \
+        int j = 0;                                                  \
+        while (isalpha (text_data[*i]))                             \
+        {                                                           \
+            str[j++] = text_data[*i];                               \
+            *i += 1;                                                \
+        }                                                           \
+        CALLOC (node->data.func, char, MAX_STR_SIZE);               \
+        strcpy (node->data.func, str);                              \
+    }
 
 #define ADD_IF_(...)                                                               \
     tokens->array_tokens[tokens->size].type = IF_;                                 \
@@ -77,12 +87,22 @@
     printf ("if [%s]\n", tokens->array_tokens[tokens->size].data.if_);             \
     tokens->size += 1;
 
-#define ADD_VARIABLE(...)                                                       \
-    tokens->array_tokens[tokens->size].type = VAR;                              \
-    CALLOC (tokens->array_tokens[tokens->size].data.var, char, MAX_STR_SIZE);   \
-    strcpy (tokens->array_tokens[tokens->size].data.var, str);                  \
-    printf ("var [%s]\n", tokens->array_tokens[tokens->size].data.var);         \
-    tokens->size += 1;
+#define ADD_OP(...)                 \
+    node->data.op = text_data[*i];  \
+    *i += 1;                        \
+
+#define ADD_VARIABLE(...)                                   \
+    {                                                       \
+        char str[MAX_STR_SIZE] = {0};                       \
+        int j = 0;                                          \
+        while (isalpha (text_data[*i]))                     \
+        {                                                   \
+            str[j++] = text_data[*i];                       \
+            *i += 1;                                        \
+        }                                                   \
+        CALLOC (node->data.var, char, MAX_STR_SIZE);        \
+        strcpy (node->data.var, str);                       \
+    }
 
 #define ADD_SIGN(...)                                                               \
     char str[MAX_OP_SIZE] = {0};                                                    \
@@ -130,12 +150,45 @@
         i++;                            \
     i++;
 
+#define SEARCH_FOR_END_TOKEN(...)                           \
+    while (text_data[i] != '{' && text_data[i] != '}')      \
+        i++;
+
+
 #define GET_NODE_TYPE(node)                 \
     int n = 0;                              \
     while (text_data[i] != '#')             \
         elem[n++] = text_data[i++];         \
     i++;                                    \
     sscanf (elem, "%d", &(node->type));     \
-    printf ("{%d}\n", node->type);
+    //printf ("{%d}\n", node->type);
+
+#define ADD_VAR_ASM(...)                                                    \
+    int flag = 0;                                                           \
+    for (int i = 0; i < variables->size; i++)                               \
+        if (!strcmp (node->data.var, variables->array_var[i].name))         \
+        {                                                                   \
+            flag = 1;                                                       \
+            printf ("var\n");                                               \
+            fprintf (asm_file, "push [%d]\n", variables->array_var[i].adr); \
+        }                                                                   \
+    if (flag == 0)                                                          \
+    {                                                                       \
+        printf ("<< Undefined variable >>\n");                              \
+        err = 1;                                                            \
+        return ERROR;                                                       \
+    }
+
+#define ADD_OP_ASM(elem)                                                \
+    if (node->left != NULL && node->left->type != DEFUALT)              \
+        make_asm_code (node->left, variables, asm_file);                \
+    if (node->left != NULL && node->right->type != DEFUALT)             \
+        make_asm_code (node->right, variables, asm_file);               \
+    fprintf (asm_file, elem);
+
+#define ADD_OP_LONG_ASM(elem)                                   \
+    if (node->left != NULL && node->left->type != DEFUALT)      \
+        make_asm_code (node->left, variables, asm_file);        \
+    fprintf (asm_file, elem);
 
 #endif // DSL_H_INCLUDED
